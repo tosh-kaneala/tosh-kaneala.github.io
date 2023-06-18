9e591b6bc9e216f2162a21be6fb870d64c4583b3
tosh-kaneala <tkbrown@uw.edu>
2023-06-17 22:09:08 -0700

    updating js and css

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-// Smooth scrolling functionality for navigation links
 document.addEventListener('DOMContentLoaded', function() {
+  // Smooth scrolling functionality for navigation links
   const smoothScrollLinks = document.querySelectorAll('a[href^="#"]');
   
   for (let smoothScrollLink of smoothScrollLinks) {
@@ -15,4 +15,47 @@ document.addEventListener('DOMContentLoaded', function() {
       });
     });
   }
+  
+  // Lazy loading for project images
+  const projectImages = document.querySelectorAll('.project-grid img');
+  
+  const lazyLoad = target => {
+    const observer = new IntersectionObserver((entries, observer) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          const img = entry.target;
+          const src = img.getAttribute('data-src');
+          
+          img.setAttribute('src', src);
+          img.classList.add('fade-in');
+          
+          observer.unobserve(img);
+        }
+      });
+    });
+    
+    observer.observe(target);
+  };
+  
+  projectImages.forEach(lazyLoad);
+  
+  // Scroll reveal animation for project overlays
+  const projectOverlays = document.querySelectorAll('.project-overlay');
+  
+  const scrollReveal = target => {
+    const observer = new IntersectionObserver((entries, observer) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          const overlay = entry.target;
+          overlay.classList.add('reveal');
+          
+          observer.unobserve(overlay);
+        }
+      });
+    });
+    
+    observer.observe(target);
+  };
+  
+  projectOverlays.forEach(scrollReveal);
 });
